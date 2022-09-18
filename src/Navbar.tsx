114c48dbef9e078fbf16b64792db1417ee6b280e
mrdooby <mrdooby@DESKTOP-OC2PQIT.localdomain>
2022-09-17 21:37:29 -0700

    feat: on hover border, larger area for nav, centered

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,47 +3,86 @@ import styled from "styled-components";
 import { Container, Grid } from "@mui/material";
 
 // styled components
-const Name = styled.div`
+const NameBox = styled.div`
   display: flex;
+  height: 100%;
+  align-items: center;
+  border-top: solid transparent;
+  :hover {
+    color: blue;
+    border-top: solid pink;
+  }
 `;
 
 const Header = styled.div`
   display: flex;
+  height: 100%;
+  align-items: center;
+  border-top: solid transparent;
+  :hover {
+    color: blue;
+    border-top: solid pink;
+  }
 `;
 
 const Resume = styled.div`
   display: flex;
+  height: 100%;
+  align-items: center;
+  border-top: solid transparent;
+  :hover {
+    color: blue;
+    border-top: solid pink;
+  }
+`;
+
+const NavbarContainer = styled.div`
+  background-color: blue;
+  width: 100%;
+  height: 5em;
 `;
 function Navbar() {
   return (
-    <Container maxWidth="lg">
-      <Grid container>
-        <Grid item border="solid black" md={3}>
-          <Name>clayton chin</Name>
+    <NavbarContainer>
+      <Container maxWidth="lg" sx={{ height: "100%", backgroundColor: "gray" }}>
+        <Grid container width="100%" height="100%">
+          <Grid
+            container
+            item
+            border="solid black"
+            md={3}
+            alignContent="center"
+          >
+            <NameBox>
+              <span>clayton chin</span>
+            </NameBox>
+          </Grid>
+          <Grid
+            container
+            item
+            border="solid blue"
+            md={6}
+            direction="row"
+            justifyContent="space-around"
+            alignContent="center"
+          >
+            <Header>about</Header>
+            <Header>projects</Header>
+            <Header>contact</Header>
+          </Grid>
+          <Grid
+            container
+            item
+            border="solid green"
+            md={3}
+            justifyContent="flex-end"
+            alignContent="center"
+          >
+            <Resume>resume</Resume>
+          </Grid>
         </Grid>
-        <Grid
-          container
-          item
-          border="solid blue"
-          md={6}
-          direction="row"
-          justifyContent="space-around"
-        >
-          <Header>about</Header>
-          <Header>projects</Header>
-          <Header>contact</Header>
-        </Grid>
-        <Grid
-          container
-          item
-          border="solid green"
-          md={3}
-          justifyContent="flex-end"
-        >
-          <Resume>resume</Resume>
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </NavbarContainer>
   );
 }
 
